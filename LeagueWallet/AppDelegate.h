fb68dfd56ae582524b9f9ff12dd17c5649065386
milosjovanovic <milos.jovanovic@cbc.ca>
2016-05-13 17:22:08 -0400

    Empty project

---
+++ LeagueWallet/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LeagueWallet
+//
+//  Created by Milos Jovanovic on 2016-05-13.
+//  Copyright © 2016 abvgd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LeagueWallet/DataViewController.h
@@ -0,0 +1,17 @@
+//
+//  DataViewController.h
+//  LeagueWallet
+//
+//  Created by Milos Jovanovic on 2016-05-13.
+//  Copyright © 2016 abvgd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DataViewController : UIViewController
+
+@property (strong, nonatomic) IBOutlet UILabel *dataLabel;
+@property (strong, nonatomic) id dataObject;
+
+@end
+
